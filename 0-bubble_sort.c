8733ae253136a8fd0c72312fc1b7cf2a3bf37786
shahad-FS <alsaneea.s@gmail.com>
2025-10-30 05:56:44 +0000

    update the code to solve the error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,14 +9,11 @@
 void bubble_sort(int *array, size_t size)
 {
 	int tmp;
-	size_t i, j;
+	size_t i, j;;
 
-	if (array == NULL || size == 0)
-		return;
-
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
