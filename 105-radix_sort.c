ee162e646108c496dffe9eb28e1ce1a47d1f0934
shahad-FS <alsaneea.s@gmail.com>
2025-10-31 10:33:21 +0000

    update the code

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -41,8 +41,8 @@ void counting_sort_digit(int *array, size_t size, int exp)
 		count[i] += count[i - 1];
 	for (i = size - 1; (int)i >= 0; i--)
 	{
-		output[count[(array[i - 1] / exp) % 10] - 1] = array[i - 1];
-		count[(array[i - 1] / exp) % 10]--;
+		output[count[(array[i] / exp) % 10] - 1] = array[i];
+		count[(array[i] / exp) % 10]--;
 	}
 	for (i = 0; i < size; i++)
 		array[i] = output[i];
@@ -67,7 +67,7 @@ void radix_sort(int *array, size_t size)
 
 	for (exp = 1; max / exp > 0; exp *= 10)
 	{
-		counting_sort_digit(array, size, exp);
+counting_sort_digit(array, size, exp);
 		print_array(array, size);
 	}
 }
